6bded8b12d00caf0eb00a9f505e21fd5e1ffec00
katW20 <k.teranm@uniandes.edu.co>
2026-08-16 20:38:27 -0500

    cambios en main

--- App/view.py
+++ App/view.py
@@ -72,7 +72,7 @@ def load_books(app):
     :type app: logic
     """
     books = logic.load_books(app,
-                             "GoodReads/books-small.csv")
+                             "GoodReads/books.csv")
     return books
 
 
@@ -98,7 +98,7 @@ def load_books_tags(app):
     :type app: logic
     """
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    book_tags = logic.load_books_tags(app, "GoodReads/books-small.csv")
+    book_tags = logic.load_books_tags(app, "GoodReads/books_tags.csv")
     return book_tags
 
 
